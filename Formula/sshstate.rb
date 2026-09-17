f27d6f95b35a736f9d503bac5ff0361f903cdb86
Mouiz Ahmed <mouiza@my.yorku.ca>
2026-09-17 02:22:08 -0400

    repin sshstate 0.1.3 to the rebuilt artifacts

--- Formula/sshstate.rb
+++ Formula/sshstate.rb
@@ -6,22 +6,22 @@ class Sshstate < Formula
   on_macos do
     on_arm do
       url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_darwin_arm64.tar.gz"
-      sha256 "e542c2432d3aa2551de5f65a4eda96570ead8e98a9d0917a558384514487c0be"
+      sha256 "da5f138e6968b53816417c8dbd141cd5ad31b31d4d834525895a43de16a3528e"
     end
     on_intel do
       url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_darwin_amd64.tar.gz"
-      sha256 "98dcb89346a109948a36bf9f2e4b5f360e4060608f66f3604c8c246d0ddb084d"
+      sha256 "b29f3b1892aad9d5a3c1fdf1bbe57e64ce80aadb3c02ac669a25be1cf81e25e3"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_linux_arm64.tar.gz"
-      sha256 "2013e9f84fca224de7012fade475da26bcc18c98c8c08073a9c33d84c3d7694c"
+      sha256 "b1e35cd567923af10cf2d250e44230507930cebbcdc0c42002606b69b7b444c9"
     end
     on_intel do
       url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_linux_amd64.tar.gz"
-      sha256 "891020ef6598b07d5af44c5ae994851787952916a52852dff1f6cebfd5881a90"
+      sha256 "d23fd0e0acdc3045b8d09f7c0003263c3f952e4637cf44410302af639dff5fc1"
     end
   end
 
