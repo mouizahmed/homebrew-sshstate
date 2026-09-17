c335985f081eea8b195ba44bcd6124db9db276da
Mouiz Ahmed <mouiza@my.yorku.ca>
2026-09-17 00:45:41 -0400

    sshstate 0.1.3

--- Formula/sshstate.rb
+++ Formula/sshstate.rb
@@ -1,28 +1,28 @@
 class Sshstate < Formula
   desc "Synchronize an SSH environment across machines"
   homepage "https://github.com/mouizahmed/sshstate"
-  version "0.1.2"
+  version "0.1.3"
   license "AGPL-3.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.2/sshstate_0.1.2_darwin_arm64.tar.gz"
-      sha256 "99f59666d7ea9884d6223b23aa082ef9e15e623a985b2ddda0eb33362d6f7f87"
+      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_darwin_arm64.tar.gz"
+      sha256 "e542c2432d3aa2551de5f65a4eda96570ead8e98a9d0917a558384514487c0be"
     end
     on_intel do
-      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.2/sshstate_0.1.2_darwin_amd64.tar.gz"
-      sha256 "ca2ce422f1bea35140896ce328decc7eb0d93a40a17565c659c744257c90f7a6"
+      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_darwin_amd64.tar.gz"
+      sha256 "98dcb89346a109948a36bf9f2e4b5f360e4060608f66f3604c8c246d0ddb084d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.2/sshstate_0.1.2_linux_arm64.tar.gz"
-      sha256 "96aed03aa1554f55174050172c4f506d437522c889ebd78b36a34a140f43476f"
+      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_linux_arm64.tar.gz"
+      sha256 "2013e9f84fca224de7012fade475da26bcc18c98c8c08073a9c33d84c3d7694c"
     end
     on_intel do
-      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.2/sshstate_0.1.2_linux_amd64.tar.gz"
-      sha256 "9a4df24a93510e85722b83a6f68845583b6a4998302b12de9408a30545b0ebbb"
+      url "https://github.com/mouizahmed/sshstate/releases/download/v0.1.3/sshstate_0.1.3_linux_amd64.tar.gz"
+      sha256 "891020ef6598b07d5af44c5ae994851787952916a52852dff1f6cebfd5881a90"
     end
   end
 
