c807df0c44f4888ed720643ae77c426cc11be493
Mouiz Ahmed <mouiza@my.yorku.ca>
2026-09-17 07:40:44 +0000

    sshstate 0.1.3

--- Formula/sshstate.rb
+++ Formula/sshstate.rb
@@ -1,6 +1,7 @@
 class Sshstate < Formula
   desc "Synchronize an SSH environment across machines"
   homepage "https://github.com/mouizahmed/sshstate"
+  version "0.1.3"
   license "AGPL-3.0-only"
 
   on_macos do
